f37f668ce69a0db7db6bdfe487932cf3c9d6562c
riceinyoursink1 <riceinyoursink@gmail.com>
2025-08-04 17:45:17 -0400

    Updates

---
+++ assets/all-wallets-D0B9CVdq.js
@@ -0,0 +1,6 @@
+import{b as o}from"./core-DjgJ4vLs.js";import"./index-DS3YiWEB.js";import"./events-D6DSQO-Y.js";import"./index.es-fvUP5lbo.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{p as allWalletsSvg};
