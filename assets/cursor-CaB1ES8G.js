1e3472225c3b879132c21363eef611914c70e09c
riceinyoursink1 <riceinyoursink@gmail.com>
2025-08-04 17:26:26 -0400

    Updates

---
+++ assets/cursor-CaB1ES8G.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-mxvy_ZdX.js";import"./index-DaO1wV6y.js";import"./events-C8dNnt0K.js";import"./index.es-B_UexBjH.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
