db121e76871a1d6a5cc70968e1a32a69d265601f
Rohan7272xyz <rohan7272@vt.edu>
2025-08-05 10:09:28 -0400

    fixed site issue

--- dist/assets/off-DI9pbFcC.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-BxLiHNod.js";import"./index-CWaST8A9.js";import"./events-zgkMCPGd.js";import"./index.es-0Fw4tofy.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M8 0a1 1 0 0 1 1 1v5.38a1 1 0 0 1-2 0V1a1 1 0 0 1 1-1ZM5.26 2.6a1 1 0 0 1-.28 1.39 5.46 5.46 0 1 0 6.04 0 1 1 0 1 1 1.1-1.67 7.46 7.46 0 1 1-8.25 0 1 1 0 0 1 1.4.28Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{p as offSvg};
